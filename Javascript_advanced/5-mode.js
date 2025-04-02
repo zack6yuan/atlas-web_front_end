4c25e3a9d58f07b67ebdd029184f6a7a71d03a9d
zack6yuan <zack6yuan@gmail.com>
2025-04-02 00:00:03 -0500

    update task 5.

--- Javascript_advanced/5-mode.js
+++ Javascript_advanced/5-mode.js
@@ -1,28 +1,47 @@
 function changeMode(size, weight, transform, background, color) {
   return function() {
-    document.body.style.fontSize = size;
+    document.body.style.fontSize = size + "px";
     document.body.style.fontWeight = weight;
-    document.body.style.transform = transform;
-    document.body.style.background = background;
+    document.body.style.textTransform = transform;
+    document.body.style.backgroundColor = background;
     document.body.style.color = color;
   }
 }
 
 function main() {
+  /* Arguments accepted for spooky, dark, and scream mode */
   const spooky = changeMode(9, 'bold', 'uppercase', 'pink', 'green');
   const darkMode = changeMode(12, 'bold', 'capitalize', 'black', 'white');
   const screamMode = changeMode(12, 'normal', 'lowercase', 'white', 'black');
 
+  /* Paragraph */
   const paragraph = document.createElement('p');
   paragraph.textContent = 'Welcome Atlas!';
 
+  /* Spooky Mode Button */
   const spookyButton = document.createElement('button');
   spookyButton.textContent = 'Spooky';
 
+  /* Dark Mode Button */
   const darkModeButton = document.createElement('button');
   darkModeButton.textContent = 'Dark mode';
 
+  /* Scream Mode Button */
   const screamModeButton = document.createElement('button');
   screamModeButton.textContent = 'Scream mode';
+
+  /* Add elements to body of the page */
+  /* Listen for click events change modes */
+
+  document.body.appendChild(paragraph);
+
+  document.body.appendChild(spookyButton);
+  spookyButton.addEventListener('click', spooky);
+
+  document.body.appendChild(darkModeButton);
+  darkModeButton.addEventListener('click', darkMode);
+
+  document.body.appendChild(screamModeButton);
+  screamModeButton.addEventListener('click', screamMode);
 }
 main()
