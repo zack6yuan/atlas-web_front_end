a4d59930a37f824a724318247aee7a08332111b1
zack6yuan <zack6yuan@gmail.com>
2025-04-02 15:28:07 -0500

    update task 3.

--- Javascript_advanced/3-classroom.js
+++ Javascript_advanced/3-classroom.js
@@ -1,14 +1,18 @@
 function createClassRoom(numbersOfStudents) {
   function studentSeat(seat) {
     return function getSeatNumber() {
-      return (seat);
+      return seat;
     }
   }
   const students = [];
   for (let x = 0; x < numbersOfStudents; x++) {
-    studentSeat = x + 1;
-    students.push(studentSeat);
+    /* Iterations + 1 */
+    let newTotal = x + 1;
+    /* Add to students array */
+    students.push(studentSeat(newTotal));
   }
   return students;
 }
+
+/* Closure */
 const classRoom = createClassRoom(10);
