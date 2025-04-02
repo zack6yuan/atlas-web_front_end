a4d6891dcdadb0877827493fc45cbd46d4cd5468
zack6yuan <zack6yuan@gmail.com>
2025-04-02 09:41:53 -0500

    start task 14.

---
+++ Javascript_advanced/14-wikipedia.js
@@ -0,0 +1,10 @@
+function createElement(data) {
+  const paragraph = document.createElement('p');
+  paragraph.textContent = 'data';
+  document.body.appendChild(paragraph);
+}
+
+function queryWikipedia(callback) {
+  const getRequest = new XMLHttpRequest();
+  getRequest.open('GET' 'https://en.wikipedia.org/w/api.php?format=json&action=query&prop=extracts&exintro&explaintext&redirects=1&titles=Stack%20Overflow&origin=*');
+}
